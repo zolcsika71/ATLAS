581e9a8ba2ecabcd3c76fcdc138f8315f699002e
zolcsika71 <bushdoctor2008@gmail.com>
2024-02-04 21:18:49 +0100

    add correctness files

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 """
 This script is used to generate answers for a set of instructions using OpenAI's GPT-4 model.
-It extracts questions from a file, generates answers for each question, and stores the question-answer pairs in a JSON file.
+It extracts questions_to_generate_answers from a file, generates answers for each question, and stores the question-answer pairs in a JSON file.
 The script iterates over 26 principle files, generating 10 answers for each question in each file.
 The generated JSON files are stored in the 'gpt4' directory.
 """
@@ -16,9 +16,9 @@ RESULTS_DIR = "gpt4"
 client = OpenAI(api_key=os.environ[API_KEY])
 
 
-def generate_answers(questions, gpt_model=DEFAULT_GPT_MODEL):
+def generate_answers(questions_to_generate_answers, gpt_model=DEFAULT_GPT_MODEL):
     """
-    Generate answers for a given set of questions
+    Generate answers for a given set of questions_to_generate_answers
     """
     return [
         client.chat.completions.create(
@@ -26,24 +26,24 @@ def generate_answers(questions, gpt_model=DEFAULT_GPT_MODEL):
         )
         .choices[0]
         .message.content
-        for question in questions
+        for question in questions_to_generate_answers
     ]
 
 
 def extract_questions_from_file(file_path):
     """
-    Extract questions from a given file
+    Extract questions_to_generate_answers from a given file
     """
     with open(file_path) as file:
         lines = file.readlines()
     return [line for line in lines if line.strip().lower().startswith("question")]
 
 
-def save_results_to_file(principle_id, question_answer_pairs):
+def save_results_to_file(principle_id, question_answer_save_result):
     """
-    Save the generated questions and answers into a JSON file
+    Save the generated questions_to_generate_answers and answers into a JSON file
     """
-    json_data = json.dumps(question_answer_pairs, indent=4)
+    json_data = json.dumps(question_answer_save_result, indent=4)
     folder_path = RESULTS_DIR
     filename = f'principle_{principle_id}.json'
     if not os.path.exists(folder_path):
